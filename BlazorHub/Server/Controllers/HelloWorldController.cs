02d8289f887169ba742a2aa6815405ee56345170
alexander.serowy <serowy@hotmail.com>
2020-08-22 00:11:04 +0200

    removed unnecessary files; added controller for prosr definition

---
+++ BlazorHub/Server/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BlazorHub.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]/prosr")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public static Task<string> GetAsync()
+        {
+            return System.IO.File.ReadAllTextAsync("HelloWorldHub.prosr");
+        }
+    }
+}
