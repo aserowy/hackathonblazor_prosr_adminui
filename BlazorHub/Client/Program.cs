a1c88866d1abfa2febc14e19611a72574fa8e23c
Stefan <stefan.suermann@adesso.de>
2020-08-21 21:09:31 +0200

    added asphosted wasm project

--- BlazorHub/Client/Program.cs
+++ BlazorHub/Client/Program.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Net.Http;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using prozr.Blazor;
+using Microsoft.Extensions.Logging;
 
-namespace prozr
+namespace BlazorHub.Client
 {
     public class Program
     {
